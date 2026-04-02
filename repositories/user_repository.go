6445d01cee6137994fa0b3d2d3d7a1c42f98fd8d
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-02 20:22:26 +0700

    Mencari User

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -7,3 +7,13 @@ type UserRepository struct{}
 func NewUserRepository() *UserRepository {
 return &UserRepository{}
 }
+
+// FindByFirebaseUID mencari user berdasarkan Firebase UID
+func (r *UserRepository) FindByFirebaseUID(uid string) (*models.User, error) {
+var user models.User
+result := config.DB.Where("firebase_uid = ?", uid).First(&user)
+if result.Error != nil {
+return nil, result.Error
+}
+return &user, nil
+}
